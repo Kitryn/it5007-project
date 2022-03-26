a0f3a12db972b107e157a0d655db448403f67a40
Ray <dingmingray@gmail.com>
2022-03-26 22:55:43 +0800

    added placeholders for homepage

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import LoginPage from './LoginPage/LoginPage';
 
 function App() {
-  return <div>Hello world!</div>;
+  return <LoginPage />;
 }
 
 export default App;
---
+++ packages/client/src/LoginPage/GoogleAuth.jsx
@@ -0,0 +1,5 @@
+export default class GoogleAuth extends React.Component {
+  render() {
+    return <div className="py-3 border">GoogleAuth PlaceHolder</div>;
+  }
+}
