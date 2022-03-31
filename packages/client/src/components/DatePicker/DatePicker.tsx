8352195f147c29fd476bd7618f9235b3a19c3393
Yixun Chen <yixun@alphalab.capital>
2022-04-01 00:31:08 +0800

    style datepicker text

--- packages/client/src/components/DatePicker/DatePicker.tsx
+++ packages/client/src/components/DatePicker/DatePicker.tsx
@@ -2,7 +2,10 @@ export default function DatePicker() {
     return (
         <form className="form-floating col-4">
             <div className="input-group">
-                <input type="date" className="form-control text-center" />
+                <input
+                    type="date"
+                    className="form-control text-center text-primary fw-bold"
+                />
             </div>
         </form>
     )
