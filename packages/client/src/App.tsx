80e4ea52ef7df4dc67e200dd799b0d1692fd10f3
Yixun Chen <yixun@alphalab.capital>
2022-03-31 19:09:51 +0800

    change routing for cashpage -- deposit and withdraw as tabbed panes instead of routes

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -53,7 +53,7 @@ function App() {
                     }
                 />
                 <Route
-                    path="/cash/deposit"
+                    path="/cash"
                     element={
                         <RequireAuth>
                             <Navbar />
