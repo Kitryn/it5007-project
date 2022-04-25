4a08351b0f50dc3bf64b2fa1f35b46b749e706d8
Ray <dingmingray@gmail.com>
2022-04-25 22:51:33 +0800

    page added

---
+++ packages/client/src/routes/LiquidityPage/Console.tsx
@@ -0,0 +1,56 @@
+import "./style.css"
+
+const Console = ({ token, selection, onSelectHander, autoFocus }) => {
+    return (
+        <div className="row m-0 bg-primary" style={{ height: "200px" }}>
+            <div className="px-5">
+                <div className="row py-3">
+                    <div className="col-3">
+                        <div className="btn-group">
+                            <button
+                                type="button"
+                                className="btn btn-light dropdown-toggle rounded-pill px-3 fw-bold                                "
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                            >
+                                {token ? token : "Select a Token"}
+                            </button>
+                            <ul className="dropdown-menu">
+                                {selection.map((option: string) => (
+                                    <li key={option}>
+                                        <a
+                                            className="dropdown-item"
+                                            href="#"
+                                            onClick={(e) => onSelectHander(e)}
+                                        >
+                                            {option}
+                                        </a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="row py-3 px-3">
+                    <div className="col">
+                        <input
+                            type="text"
+                            className="form-control-lg border-0 bg-primary fw-bold text-white p-0 m-0"
+                            maxLength={9}
+                            aria-label="Amount"
+                            placeholder="0.00"
+                            autoFocus={autoFocus}
+                        ></input>
+                    </div>
+                    <div className="col-2">
+                        <p className="text-white fs-4 balance">
+                            <span className="">Balance: 0.00</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Console
--- packages/client/src/routes/LiquidityPage/LiquidityPage.tsx
+++ packages/client/src/routes/LiquidityPage/LiquidityPage.tsx
@@ -1,5 +1,91 @@
+import { useState } from "react"
+import PushableButton from "../../components/UI/PushableButton/PushableButton"
+import Console from "./Console"
+import "./style.css"
 const LiquidityPage = () => {
-    return <></>
+    const selection = ["BTC", "ETH", "COI", "KJS"]
+    const [token1, setToken1] = useState("")
+    const [token2, setToken2] = useState("")
+
+    function onSelectHanderToken1(e: any) {
+        setToken1(e.target.innerText)
+    }
+    function onSelectHanderToken2(e: any) {
+        setToken2(e.target.innerText)
+    }
+
+    return (
+        <>
+            <div className="container liquidity">
+                <div className="card">
+                    <div className="row m-0">
+                        <div className="col border-end py-3">
+                            <span
+                                className="w-100 btn btn-white fs-1 text-dark fw-bold"
+                                style={{ cursor: "auto" }}
+                            >
+                                Add Liquidity
+                            </span>
+                        </div>
+                    </div>
+                    <Console
+                        token={token1}
+                        selection={selection}
+                        onSelectHander={onSelectHanderToken1}
+                        autoFocus={true}
+                    ></Console>
+                    <div className="row m-0 py-3"></div>
+                    <Console
+                        token={token2}
+                        selection={selection}
+                        onSelectHander={onSelectHanderToken2}
+                        autoFocus={false}
+                    ></Console>
+                    <div className="row py-3">
+                        {token1 && token2 ? (
+                            <>
+                                <div className="col-2"></div>
+                                <div className="col-8">
+                                    <div className="row border-bottom text-muted fs-4 ">
+                                        <div className="col">
+                                            <p className="text-start">Rate</p>
+                                        </div>
+                                        <div className="col">
+                                            <p className="text-end">
+                                                <span>1 {token1} </span>
+                                                <span>=</span>
+                                                <span>0.000001 {token2}</span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className="row text-muted fs-4 pt-3">
+                                        <div className="col">
+                                            <p className="text-start">
+                                                Share of pool
+                                            </p>
+                                        </div>
+                                        <div className="col">
+                                            <p className="text-end">0.02%</p>
+                                        </div>
+                                    </div>
+                                    <div className="row pt-3">
+                                        <PushableButton>Approve</PushableButton>
+                                    </div>
+                                </div>
+                                <div className="col-2"></div>
+                            </>
+                        ) : (
+                            <div className="row text-center p-1 ">
+                                <span className="text-secondary ">
+                                    <em>Please select a token to begin</em>
+                                </span>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default LiquidityPage
